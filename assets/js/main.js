e5b686d57c99273814a9e244eea2cb7eac877c48
aldiaza <alr.diaza@gmail.com>
2022-06-05 21:40:31 -0400

    completed template

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,34 @@ d.addEventListener('scroll', () => {
 toUp.addEventListener('click', () => {
     valueScroll >= 800 ? window.scrollTo(0, 0) : null;
 });
+
+let form = d.querySelector('#contact');
+const send = async () => {
+    const email = d.querySelector('#email');
+    const subject = d.querySelector('#subject');
+    const message = d.querySelector('#message');
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email.value,
+            subject: subject.value,
+            message: message.value,
+        }),
+    };
+    data = await fetch(
+        'https://mail-sender-serv.herokuapp.com/mailing',
+        config
+    );
+    console.log(await data.json());
+    email.value = '';
+    subject.value = '';
+    message.value = '';
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    send();
+});
